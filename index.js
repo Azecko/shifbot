0eb0754755f4cc44322bc5030ffbcfafb7c4c68e
Azecko <30987143+Azecko@users.noreply.github.com>
2018-03-19 17:02:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,67 @@ bot.on("ready", function () {
     console.log("Je suis prêt à me rendre sur " + bot.guilds.size + " serveur(s) ! Sous le pseudo de " + bot.user.username + " !");
 });
 
+bot.on("guildMemberAdd", member => {
+    db.fetchObject(`autoRole_${member.guild.id}`).then(i => {
+        
+                // Check if no role is given
+                if (!i.text || i.text.toLowerCase() === 'none'); // We want to put this un our guildMemberAdd, but we want to delete the return statement and just replace it with ; so it can run the rest of the code
+                else { // Run if a role is found...
+        
+                    try { // Try to add role...
+                        member.addRole(member.guild.roles.find('name', i.text))
+                    } catch (e) { // If an error is found (the guild supplied an invalid role), run this...
+                        console.log("Un serveur a essayer d'ajouter un role inexistant à un utilisateur.") // You can commet this line out if you don't want this error message
+                    }
+        
+                }
+            })
+    db.fetchObject(`messageChannel_${member.guild.id}`).then(i => {
+        
+                    // Fetch Welcome Message (DMs)
+                    db.fetchObject(`joinMessageDM_${member.guild.id}`).then(o => {
+        
+                        // DM User
+                        if (!o.text) console.log('Error: Join DM Message not set. Please set one using ~setdm <message>'); // This will log in console that a guild didn't set this up, you dont need to include the conosle.log
+                        else member.send(o.text.replace('{user}', member.toString()).replace('{members}', member.guild.memberCount)) // This is where the embed function comes in, as well as replacing the variables we added earlier in chat.
+        
+                        // Now, return if no message channel is defined
+                        if (!member.guild.channels.get(i.text)) return console.log('Error: Welcome/Leave channel not found. Please set one using ~setchannel #channel') // Again, this is optional. just the console.log not the if statement, we still want to return
+        
+                        // Fetch the welcome message
+                        db.fetchObject(`joinMessage_${member.guild.id}`).then(p => {
+        
+                            // Check if they have a join message
+                            if (!p.text) console.log('Error: User Join Message not found. Please set one using ~setwelcome <message>')
+                            else member.guild.channels.get(i.text).send(p.text.replace('{user}', member).replace('{members}', member.guild.memberCount)) // We actually want to send the message.
+        
+                        })
+        
+                    })
+        
+                })
+})
+
+bot.on("guildMemberRemove", function(member) {
+    db.fetchObject(`messageChannel_${member.guild.id}`).then(i => {
+        
+                    // Fetch Welcome Message (DMs)
+        
+                        // Now, return if no message channel is defined
+                        if (!member.guild.channels.get(i.text)) return console.log('Error: Welcome/Leave channel not found. Please set one using ~setchannel #channel') // Again, this is optional. just the console.log not the if statement, we still want to return
+        
+                        // Fetch the welcome message
+                        db.fetchObject(`leaveMessage_${member.guild.id}`).then(o => {
+        
+                            // Check if they have a join message
+                            if (!o.text) console.log('Error: User Join Message not found. Please set one using +setleave <message>')
+                            else member.guild.channels.get(i.text).send(o.text.replace('{user}', member).replace('{members}', member.guild.memberCount)) // We actually want to send the message.
+        
+                        })
+        
+                    })
+});
+
 bot.on("message", async function(message) {
 
     if (message.author.equals(bot.user)) return;
